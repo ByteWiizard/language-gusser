67e79dd15924cdaf686acd4260206dd97dacaf27
Byte Wizard <yamangoyal100@gmail.com>
2023-07-26 17:32:11 +0530

    Main Code

---
+++ app.mjs
@@ -0,0 +1,62 @@
+import { franc, francAll } from 'franc'
+import lang from 'langs'
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    const detect = document.getElementById('Detect');
+    const sentence = document.getElementById('to_detect');
+    const overlay = document.querySelector('#overlay');
+    const display = document.querySelector('#display');
+    const backContent = document.querySelector('#container');
+    const textDisplay = document.querySelector('#display #text-desc')
+    const LanguageDisplay = document.querySelector('#display #lang-desc')
+
+
+    detect.addEventListener('click', function () {
+
+
+        if (sentence.value === '') {
+            alert('Please Enter Some Text to Detect');
+        }
+        else {
+
+            const sent = sentence.value;
+            const langCode = franc(sent);
+
+            if (langCode === 'und') {
+                alert(`Can't Detect Language Please Be More Descriptive`);
+            } else {
+
+                var language;
+                if(langCode === 'zyb'){
+                    LanguageDisplay.innerText = "Yongbei Zhuang"
+                }else{
+                    language = lang.where("3", langCode);
+                }
+                
+
+                display.style.zIndex = '1';
+                display.style.top = '50%';
+                backContent.style.zIndex = '-1';
+                overlay.style.opacity = 0.5;
+                textDisplay.innerText = sentence.value;
+
+                if(language.name != undefined){
+                    LanguageDisplay.innerText = language.name;
+                }
+                
+
+
+
+
+            }
+        }
+
+
+    })
+})
+
+
+
